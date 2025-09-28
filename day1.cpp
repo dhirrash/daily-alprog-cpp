021e3a57c238689ddaf530546737ca11fbb20e2f
dhirrash <nadhirrasyad@gmail.com>
2025-09-29 00:40:17 +0700

    hellothere

---
+++ day1.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+
+/*int main(){
+int tinggi, panjang;
+std::cin >> tinggi;
+std::cin >> panjang;
+
+for(int i = 1; i <= tinggi; i++){
+    for(int j = 1; j <= panjang; j++){
+    std::cout << "*";
+    }
+    std::cout << '\n';
+}
+
+return 0;
+}
+*/
+
+
+/* buat pola dan huruf:
+persegi : batas sama
+persegi panjang : batas beda
+
+
+
+
+
+*/
+#include <iostream>
+
+void buatA(int x){
+    int panjang = 5 * x;
+
+    for(int i = 0; i < x; i++){
+        for(int j = 0; j < panjang; j++)
+        std::cout << "A";
+        std::cout << '\n';
+    }
+
+    for(int i = 0; i < x; i++){
+        for(int j = 0; j < x; j++)
+        std::cout << "A";
+
+        for(int j = 0 + x; j < panjang - x; j++)
+        std::cout << " ";
+        
+        for(int j = 0; j < x; j++)
+        std::cout << "A";
+        std::cout << '\n';
+    }
+    for(int i = 0; i < x; i++){
+        for(int j = 0; j < panjang; j++)
+        std::cout << "A";
+        std::cout << '\n';
+    }
+    for(int i = 0; i < x * 2; i++){
+        for(int j = 0; j < x; j++)
+        std::cout << "A";
+
+        for(int j = 0 + x; j < panjang - x; j++)
+        std::cout << " ";
+        
+        for(int j = 0; j < x; j++)
+        std::cout << "A";
+        std::cout << '\n';
+    }  
+}
+
+void buatB(int x){
+    int panjang = 5 * x;
+
+for(int i = 0; i <= x; i++){
+    for(int j = 0; j <= panjang; j++)
+    std::cout << "B";
+    std::cout << '\n';
+
+for(int i = 0; i <= x; i++){
+    for(int j = 0; j <= panjang; j++)
+    std::cout << "B";
+
+    for(int k = 0; k + x <= panjang - x;k++)
+    std::cout << " ";
+    }
+}
+
+}
+
+
+int main(){
+    int x;
+    std::cin >> x;
+    buatB(x);
+
+
+    return 0;
+}

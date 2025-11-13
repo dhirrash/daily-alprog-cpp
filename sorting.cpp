27a6de2fc0b5c7816453b1f853c78177fde04928
dhirrash <nadhirrasyad@gmail.com>
2025-11-13 22:53:39 +0700

    menambahkansorting

--- sorting.cpp
+++ sorting.cpp
@@ -1,12 +1,57 @@
 #include <iostream>
 using namespace std;
 
+void inputData(int &arrSize, int arr[]);
+void menu(int arrSize, int arr[]);
+void swap(int &a, int &b);
+void bubbleSort(int arrSize, int arr[]);
+void selectionSort(int arrSize, int arr[]);
+void outputData(int arrSize, int arr[]);
+
+int main()
+{
+    int arrSize;
+    int arr[100];
+    inputData(arrSize, arr);
+    menu(arrSize, arr);
+    outputData(arrSize, arr);
+    return 0;
+}
+
 void inputData(int &arrSize, int arr[])
 {
+    cout << "Input array size: ";
     cin >> arrSize;
+
+    cout << "Input array elements: ";
     for(int i = 0; i < arrSize; i++)
     {
         cin >> arr[i];
+    }
+}
+
+void menu(int arrSize, int arr[])
+{
+    int choice;
+    cout << "\n----- Choose sorting method -----\n";
+    cout << "1. Bubble Sort\n";
+    cout << "2. Selection Sort\n";
+    cout << "Your choice: ";
+    cin >> choice;
+
+    switch (choice)
+    {
+        case 1:
+            bubbleSort(arrSize, arr);
+            cout << "\nArray sorted using Bubble Sort.\n";
+            break;
+        case 2:
+            selectionSort(arrSize, arr);
+            cout << "\nArray sorted using Selection Sort.\n";
+            break;
+        default:
+            cout << "Invalid choice!\n";
+            break;
     }
 }
 
@@ -17,36 +62,45 @@ void swap(int &a, int &b)
     b = temp;
 }
 
-void bubbleSort(int &a, int arr[])
+void bubbleSort(int arrSize, int arr[])
 {
-    for(int i = 0; i < a - 1; i++)
+    for(int i = 0; i < arrSize - 1; i++)
     {
-        for(int j = 0; j < a - i - 1; j++)
+        for(int j = 0; j < arrSize - i - 1; j++)
         {
-            if(arr[j] > arr [j + 1])
+            if(arr[j] > arr[j + 1])
             {
-                swap(arr[j], arr[j +1]);
+                swap(arr[j], arr[j + 1]);
             }
         }
     }
 }
 
-void outputData(int a, int arr[])
+void selectionSort(int arrSize, int arr[])
 {
-    for(int i = 0; i < a; i++)
+    for(int i = 0; i < arrSize - 1; i++)
     {
-        cout << arr[i] << " ";
+        int minIndex = i;
+        for(int j = i + 1; j < arrSize; j++)
+        {
+            if(arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        if(minIndex != i)
+        {
+            swap(arr[i], arr[minIndex]);
+        }
     }
 }
 
-int main()
+void outputData(int arrSize, int arr[])
 {
-    int a;
-    int arr[100];
-    inputData(a, arr);
-    bubbleSort(a, arr);
-    outputData(a, arr);
-
-    return 0;
+    cout << "\nSorted array: ";
+    for(int i = 0; i < arrSize; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
-
